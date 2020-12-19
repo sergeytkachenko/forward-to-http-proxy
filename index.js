a2f7f957784e2cb40f1b7f5de4368f18bb510e60
sergey.tkachenko <serg.tkachenko@hotmail.com>
2020-12-19 19:18:18 +0200

    change port

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const server = http.createServer();
 server.on('error', err => console.error(err))
 server.on('connect', (req, socket) => connect(req, socket));
 server.on('request', (req, res) => request(req, res));
-server.listen(8000);
+const port = parseInt(process.env.PORT) || 80;
+server.listen(port);
 process.on('SIGINT', function() {
 	console.log("Caught interrupt signal");
 	process.exit();
 });
-console.log(`SERVER listen on 8000 port...`);
+console.log(`SERVER listen on ${port} port...`);
